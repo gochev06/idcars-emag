ac7c2e802027dc22909b8a78bf4580bfa4218103
Kostadin <k.gochev1606@gmail.com>
2025-04-03 20:35:36 +0300

    update to not reload the database on push

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import urllib.parse
+
 from dotenv import load_dotenv
 
 # Load environment variables from .env file if available
@@ -10,13 +12,10 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     EMAG_API_KEY = os.environ.get("EMAG_API_KEY")
     FITNESS1_API_KEY = os.environ.get("FITNESS1_API_KEY")
-    # NEW_EMAG_API_KEY = os.environ.get("NEW_EMAG_API_KEY")  # TODO: remove it for prod
     # Additional configuration options can be added here
 
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI ", "sqlite:///app.db"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask app configuration
