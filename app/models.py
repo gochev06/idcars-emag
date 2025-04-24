3489e644bc4933b7fcb59573e77632ec74a30e93
Kostadin <k.gochev1606@gmail.com>
2025-04-24 08:49:59 +0300

    add fields to the models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class FitnessCategory(db.Model):
     __tablename__ = "fitness_categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+    emag_category_id = db.Column(db.Integer, nullable=True)
+    emag_product_name_category = db.Column(db.String(255), nullable=True)
 
     def as_dict(self):
         return {"id": self.id, "name": self.name}
@@ -29,6 +31,7 @@ class Mapping(db.Model):
             "id": self.id,
             "fitness1_category": self.fitness1_category,
             "emag_category": self.emag_category,
+            "emag_category_id": self.emag_category_id,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat(),
         }
